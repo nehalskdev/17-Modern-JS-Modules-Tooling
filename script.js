5749dc13767f6865317a49d8a88e7ebba615d382
nehalskdev <nikesk1999@gmail.com>
2024-09-27 19:45:22 +0530

    learning top level await in module

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // Importing module
-import './shoppingCard.js';
+// import { addToCart, totalPrice as price, qt } from './shoppingCard.js';
+
+// addToCart('bread', 5);
+// console.log(price, qt);
+
 console.log('importing module');
+
+// import * as shopingCart from './shoppingCard.js';
+// shopingCart.addToCart('bread', 5);
+
+import add, { cart } from './shoppingCard.js';
+add('pizza', 2);
+add('apple', 4);
+add('bread', 5);
+
+console.log(cart);
+
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
--- shoppingCard.js
+++ shoppingCard.js
@@ -2,5 +2,20 @@
 console.log('exporting module');
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as qt };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
