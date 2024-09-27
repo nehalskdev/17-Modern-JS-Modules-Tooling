a1f7ff6d35e689de9df92da0123ad963554734cb
nehalskdev <nikesk1999@gmail.com>
2024-09-27 20:15:08 +0530

    learned module pattern

--- script.js
+++ script.js
@@ -4,36 +4,61 @@
 // addToCart('bread', 5);
 // console.log(price, qt);
 
-console.log('importing module');
+// console.log('importing module');
 
-// import * as shopingCart from './shoppingCard.js';
-// shopingCart.addToCart('bread', 5);
+// // import * as shopingCart from './shoppingCard.js';
+// // shopingCart.addToCart('bread', 5);
 
-import add, { cart } from './shoppingCard.js';
-add('pizza', 2);
-add('apple', 4);
-add('bread', 5);
+// import add, { addToCart, cart } from './shoppingCard.js';
+// add('pizza', 2);
+// add('apple', 4);
+// add('bread', 5);
 
-console.log(cart);
+// console.log(cart);
 
-// console.log('start fetching');
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
-// console.log('something');
+// // console.log('start fetching');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // console.log('something');
 
-const getLastPost = async function () {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await res.json();
-    console.log(data);
-    return { title: data.at(-1).title, text: data.at(-1).body };
-};
-const lastPost = getLastPost();
-console.log(lastPost);
+// const getLastPost = async function () {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json();
+//     console.log(data);
+//     return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+// const lastPost = getLastPost();
+// console.log(lastPost);
 
 // not very clean
 // lastPost.then(last => console.log(last));
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
 
+const shoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity) {
+      cart.push({ product, quantity });
+      console.log(`${quantity} ${product} added to cart`);
+    };
+
+    const orderStock = function (product, quantity) {
+      console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
